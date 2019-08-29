5b1c42a0f9548dcc3935ae69405b1e7f0dd040ce
songyuhong <2814277263@qq.com>
2019-08-29 20:26:11 +0800

    该

--- elm_app/src/components/About.vue
+++ elm_app/src/components/About.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="about">
     <div class="footer">
-           <router-link to="/about/Takeaway" @click.native="num=0" class="link">
-               <i class="iconfont" :class="{active:num==0}">&#xe603;</i>
-               <p>外卖</p>
-           </router-link>
-            <router-link @click.native="num=1" to="/about/seach" class="link">
-                <i class="iconfont" :class="{active:num==1}">&#xe600;</i>
-                <p>搜索</p>
-            </router-link>
-           <router-link to="/about/Order" @click.native="num=2" class="link">
-               <i class="iconfont" :class="{active:num==2}">&#xe601;</i>
-               <p>订单</p>
-           </router-link>
-            <router-link to="/about/mine" @click.native="num=3" class="link">
-                <i class="iconfont" :class="{active:num==3}">&#xe602;</i>
-                <p>我的</p>
-            </router-link>
+        <router-link to="/about/Takeaway" @click.native="num=0" class="link">
+            <i class="iconfont" :class="{active:num==0}">&#xe603;</i>
+            <p>外卖</p>
+        </router-link>
+        <router-link @click.native="num=1" to="/about/seach" class="link">
+            <i class="iconfont" :class="{active:num==1}">&#xe600;</i>
+            <p>搜索</p>
+        </router-link>
+        <router-link to="/about/Order" @click.native="num=2" class="link">
+            <i class="iconfont" :class="{active:num==2}">&#xe601;</i>
+            <p>订单</p>
+        </router-link>
+        <router-link to="/about/mine" @click.native="num=3" class="link">
+            <i class="iconfont" :class="{active:num==3}">&#xe602;</i>
+            <p>我的</p>
+        </router-link>
     </div>
     <router-view></router-view>
   </div>
@@ -46,25 +46,20 @@
     right: 0;
     bottom: 0;
     box-shadow: 0 -0.02667rem 0.05333rem rgba(0,0,0,.1);
+    font-size: 0.2rem;
   }
-  .footer div{
-      width: 25%;
-      float: left;
-      font-size: 16px;
-      height: 100%;
-      padding: 0.05rem 0;
-  }
+
   .active{
       color: rgb(49, 144, 232);
   }
   .footer .iconfont{
-      font-size: 0.4rem;
+      font-size: 0.46rem;
   }
   .link{
     text-decoration: none;
     display: inline-block;
     width: 25%;
     color: #666;
-    font-size:0.2rem;
+    font-size:0.16rem;
   }
 </style>
