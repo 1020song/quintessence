586c29b275a7dcb200732974146a2f105504bb02
Baoqianqian <2107624791@qq.com>
2019-09-02 19:33:33 +0800

    gai

--- elm_app/vue.config.js
+++ elm_app/vue.config.js
@@ -1,11 +1,14 @@
-module.exports = {
-    devServer: {
-        proxy: {
-            '/v4': {
-                target: 'https://elm.cangdu.org/',
-                ws: true,
-                changeOrigin: true
-            }
-        }
-    }
-}
+
+
+
+// module.exports = {
+//     devServer: {
+//         proxy: {
+//             '/v4': {
+//                 target: 'https://elm.cangdu.org/',
+//                 ws: true,
+//                 changeOrigin: true
+//             }
+//         }
+//     }
+// }
