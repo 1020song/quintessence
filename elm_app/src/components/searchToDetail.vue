59c572b4fc3c6a9c8fae0e8306ff46e9a8fc7741
Baoqianqian <2107624791@qq.com>
2019-09-04 09:27:53 +0800

    search

---
+++ elm_app/src/components/searchToDetail.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    searchToDetail
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+</style>
--- elm_app/src/router.js
+++ elm_app/src/router.js
@@ -2,41 +2,27 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import login from './views/login.vue' /*登录 lz*/
 import change from './views/changepassword.vue' /*重置密码 lz*/
-import info from './views/info.vue' /*用户信息 lz*/
-import setusername from './views/setusername.vue'
-import add from './views/address.vue'
-import addsss from './views/add.vue'
-import addss from './views/addDetail.vue'
-import adds from './views/adds.vue'
-
+import info from './views/info.vue' /*用户信息 lz */
 import Index from './views/Index.vue'
 import About from './views/About.vue'
 import City from './views/City.vue'
-import Users from './views/user.vue'
 import Takeaway from './components/Takeaway'
 import search from './components/search'
 import Order from './components/Order'
 import user from './components/user'
-// 金额
-import Balance from './views/balance.vue'
-import Balance1 from './views/balance1.vue'// 金额数量
-import Explain from './views/explain.vue'// 金额说明
 
-// 积分
-import integral from './views/score.vue'
-import integral1 from './views/score1.vue'
-import integralDetails from './views/score_details.vue'
-// Vip
-import Vip from './views/vip.vue'
-import vip1 from './views/vip1.vue'
-import vipDescription from './views/vipDescription.vue'
-import payment from './views/payment.vue'
-// 服务中心
+import foodDetail from './views/foodDetail.vue'
+import Users from './views/user.vue'
+import Balance from './views/balance.vue'
+import Balance1 from './views/balance1.vue'
+import Explain from './views/explain.vue'
 import Service from './views/service_cont.vue'
 import serviceD from './views/service_details.vue'
 import service1 from './views/service1.vue'
-// app
-import App from './views/appdetails.vue'
+import sweetmeats from './views/sweetmeats.vue'
+
+import searchtodetail from './components/searchToDetail.vue' /*搜索的时候，点击li，进入详情页*/
+
 Vue.use(Router)
 
 export default new Router({
@@ -44,20 +30,30 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
+      // 商品详情
+      path: '/foodDetail',
+      name: 'foodDetail',
+      component: foodDetail
+    },
+    // 登录
+    {
       path: '/login',
       name: 'login',
       component: login
     },
+    // 重置密码
     {
       path: '/change',
       name: 'change',
-      component: change,
+      component: change
     },
+    // 首页
     {
       path: '/',
       name: 'index',
       component: Index
     },
+    // 城市列表
     {
       path: '/city/:id',
       name: 'city',
@@ -68,24 +64,44 @@ export default new Router({
       name: 'about',
       component: About,
       children: [
+        // 外卖
         {
           path: 'Takeaway',
           props: route => ({
             name: route.query.geohash
           }),
           name: 'Takeaway',
-          component: Takeaway
+          component: Takeaway,
         },
+        {
+          path: 'sweetmeats',
+          name: 'sweetmeats',
+          component: sweetmeats
+        },
+        // 搜索
         {
           path: 'search',
           name: 'search',
-          component: search
+          component: search,
+          props: route => ({
+            name: route.query
+          }),
+          children: [
+            // 点击每一个li，进入食物详情
+            {
+              path: 'searchdetail',
+              name: 'searchdetail',
+              component: searchtodetail
+            }
+          ]
         },
+        // 订单
         {
           path: 'Order',
           name: 'Order',
           component: Order
         },
+        // 我的
         {
           path: 'user',
           name: 'user',
@@ -96,40 +112,16 @@ export default new Router({
               name: 'users',
               component: Users
             },
+            // 账户信息
             {
               path: 'info',
-              component: info,
-            },
-            {
-              path: 'setusername',
-              name: 'setusername',
-              component: setusername
-            },
-            {
-              path: 'add',
-              component: add
-            },
-            {
-              path: 'adds',
-              name: 'adds',
-              component: adds,
-              children:[
-                {
-                  path: '/',
-                  name: 'adds',
-                  component: addsss
-                },
-                {
-                  path: 'addss',
-                  component: addss
-                }
-              ]
+              component: info
             }
-            
           ]
         }
       ]
     },
+    // 我的余额
     {
       path: '/balance',
       name: 'balance',
@@ -140,31 +132,15 @@ export default new Router({
           name: 'explain',
           component: Balance1
         },
+        // 余额说明
         {
           path: 'explain',
           name: 'explain',
           component: Explain
         }
       ]
-    },
-    {
-      path: '/integral',
-      name: 'integral',
-      component: integral,
-      children: [
-        {
-          path: '/',
-          name: 'integral1',
-          component: integral1
-        },
-        {
-          path: 'details',
-          name: 'details1',
-          component: integralDetails
-        }
-      ]
-    },
-    {
+    }, {
+      // 服务中心
       path: '/service',
       name: 'service',
       component: Service,
@@ -174,39 +150,13 @@ export default new Router({
           name: 'service',
           component: service1
         },
+        // 热门问题
         {
           path: 'Details',
           name: 'details',
           component: serviceD
         }
       ]
-    },
-    {
-      path: '/vip',
-      name: 'vip',
-      component: Vip,
-      children: [
-        {
-          path: '/',
-          name: 'vip1',
-          component: vip1
-        },
-        {
-          path: 'vipDescription',
-          name:'vipDescription',
-          component: vipDescription
-        },
-        {
-          path: 'payment',
-          name: 'payment',
-          component: payment
-        }
-      ]
-    },
-    {
-      path: '/app',
-      name: 'app',
-      component: App
     }
   ]
 })
