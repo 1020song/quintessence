a242ab48f19615e2bdb8f1ad47b6b7836896ce89
lijun998 <1321721510@qq.com>
2019-09-09 14:08:11 +0800

    改

--- elm_app/src/views/service_details.vue
+++ elm_app/src/views/service_details.vue
@@ -5,13 +5,13 @@
 		<template v-slot:center>{{$route.params.tit}}</template>
 		<template v-slot:right></template>
 		</elmHead>
-		<div v-if="data.img" class="contain">
-			<div v-for="(i,$index) in data.img" :key="$index" >
+		<div v-if="datas.img!=''" class="contain">
+			<div v-for="(i,$index) in datas.img" :key="$index" >
 			<img :src="i" alt="" style="width:100%">
-			<p style="padding:.2rem;font-size:.3rem" v-html="data.info[$index]"></p>
+			<p style="padding:.2rem;font-size:.3rem" v-html="datas.info[$index]"></p>
 			</div>
 		</div>
-		<div else>
+		<div v-else class="contain">
 			<p  style="padding:.2rem;font-size:.3rem" v-html="$route.params.info"></p>
 		</div>
 		<router-view></router-view>
@@ -25,7 +25,7 @@ export default {
 	},
 	data(){
 		return{
-			data:{}
+			datas:{}
 		}
 	},
 	created(){
@@ -35,8 +35,8 @@ export default {
 			Img.push(element.split(') ##')[0])
 			Info.push(element.split(') ##')[1])
 		});
-		this.data.img=Img.slice(1)
-		this.data.info=Info.slice(1)
+		this.datas.img=Img.slice(1)
+		this.datas.info=Info.slice(1)
 	}
 }
 </script>
